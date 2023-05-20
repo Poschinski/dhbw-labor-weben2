7ea7a2cfa2e37bda4ccc37bb2a1d29c7995ab3aa
Poschinski <marcus.unglert@web.de>
2023-05-20 21:35:32 +0200

    first setup try

--- src/main/java/de/dhbw/tim/musikkatalog/controller/MusiktitelController.java
+++ src/main/java/de/dhbw/tim/musikkatalog/controller/MusiktitelController.java
@@ -1,4 +1,24 @@
 package de.dhbw.tim.musikkatalog.controller;
 
+import de.dhbw.tim.musikkatalog.model.Musiktitel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class MusiktitelController {
+
+    @GetMapping("/songs")
+    public ResponseEntity<Musiktitel> getAllSongs() {
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<String> testResponse() {
+        return new ResponseEntity<>("Helo", HttpStatus.OK);
+    }
+
 }
--- src/main/java/de/dhbw/tim/musikkatalog/model/Musiktitel.java
+++ src/main/java/de/dhbw/tim/musikkatalog/model/Musiktitel.java
@@ -1,5 +1,73 @@
 package de.dhbw.tim.musikkatalog.model;
 
+//import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+//@Entity
+//@Table(name = "musiktitel")
 public class Musiktitel {
 
+    //@Id
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+    private String album;
+    private String artist;
+    private String genre;
+    private LocalDate releaseDate;
+
+    public Musiktitel() {}
+
+    public Musiktitel(String title, String album, String artist, String genre, LocalDate releaseDate) {
+        this.title = title;
+        this.album = album;
+        this.artist = artist;
+        this.genre = genre;
+        this.releaseDate = releaseDate;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
 }
