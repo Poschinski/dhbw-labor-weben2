17b17074213a4c3ca03ad89cbdfc00f485925b1c
Poschinski <marcus.unglert@web.de>
2023-05-22 13:02:37 +0200

    add basic http methods

--- src/main/java/de/dhbw/tim/musikkatalog/controller/MusiktitelController.java
+++ src/main/java/de/dhbw/tim/musikkatalog/controller/MusiktitelController.java
@@ -3,17 +3,32 @@ package de.dhbw.tim.musikkatalog.controller;
 import de.dhbw.tim.musikkatalog.model.Musiktitel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class MusiktitelController {
 
     @GetMapping("/songs")
-    public ResponseEntity<Musiktitel> getAllSongs() {
+    public ResponseEntity<List<Musiktitel>> getAllSongs() {
         return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+
+    @GetMapping("/songs({id}")
+    public ResponseEntity<Musiktitel> getSongById(@PathVariable("id") long id) {
+        return null;
+    }
+
+    @PostMapping("/songs")
+    public ResponseEntity<Musiktitel> createSong(@RequestBody Musiktitel song) {
+        return null;
+    }
+
+    @DeleteMapping("/songs/{id}")
+    public ResponseEntity<HttpStatus> deleteSong(@PathVariable("id") long id) {
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @GetMapping("/test")
